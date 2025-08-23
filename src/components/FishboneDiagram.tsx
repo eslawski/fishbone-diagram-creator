b60b90486c212eea23aa94ca24ff985f194e6df6
Evan Slawski <evan.slawski@ithaka.org>
2025-08-23 11:20:07 -0400

    Hack for rending problem correctly

--- src/components/FishboneDiagram.tsx
+++ src/components/FishboneDiagram.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAppSelector } from "../hooks";
 import { type Cause } from "../slices/fishboneSlice";
 
-import Fishbone from '@hophiphip/react-fishbone';
-import '@hophiphip/react-fishbone/style.css';
+import Fishbone from "@hophiphip/react-fishbone";
+import "@hophiphip/react-fishbone/style.css";
 
 interface FishboneNode {
   label: string;
@@ -13,7 +13,7 @@ interface FishboneNode {
 function transformCausesToNodes(causes: Cause[]) {
   if (!Array.isArray(causes)) return [];
 
-  return causes.map(cause => {
+  return causes.map((cause) => {
     const node: FishboneNode = { label: cause.name };
 
     if (cause.causes && cause.causes.length > 0) {
@@ -24,23 +24,51 @@ function transformCausesToNodes(causes: Cause[]) {
   });
 }
 
+// Root node does not handle long text well, so hacking away the overflow styles as a workaround
+function rootNodeHack(problem: string) {
+  setTimeout(() => {
+    const span = Array.from(document.querySelectorAll("span")).find((el) => el?.textContent?.includes(problem));
+    const parent = span?.closest(".react-flow__node");
+    removeTextOverflowStyles(parent as HTMLElement);
+  });
+}
+
+function removeTextOverflowStyles(node: HTMLElement) {
+  if (!node) return;
+
+  // Remove the styles if they exist
+  if (node.style) {
+    node.style.overflow = '';
+    node.style.whiteSpace = '';
+  }
+
+  // Recursively process children
+  node.childNodes.forEach(child => removeTextOverflowStyles(child as HTMLElement));
+}
+
 const FishboneDiagram: React.FC = () => {
   // The `state` arg is correctly typed as `RootState` already
   const causes = useAppSelector((state) => state.fishbone.causes);
   const problem = useAppSelector((state) => state.fishbone.problem);
 
   const items: FishboneNode = {
-    label: "Problem",
+    label: problem,
     children: transformCausesToNodes(causes || []),
-  }
+  };
 
   return (
-    <div style={{width: "100%", height: "100%"}}>
-      <Fishbone items={items} reactFlowProps={{
-        onNodeMouseEnter: (event, node) => {
-          console.log(node);
-        }
-      }} />
+    <div style={{ width: "100%", height: "100%" }}>
+      <Fishbone
+        items={items}
+        reactFlowProps={{
+          onNodeMouseEnter: (event, node) => {
+            console.log(node);
+          },
+          onViewportChange: () => {
+            rootNodeHack(problem);
+          },
+        }}
+      />
     </div>
   );
 };
