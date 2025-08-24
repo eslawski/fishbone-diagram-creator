9aa96255b82307cc05e736b44f8bfd86bf79bdeb
Evan Slawski <evan.slawski@ithaka.org>
2025-08-24 16:24:30 -0400

    Remove unused code

--- src/slices/fishboneSlice.ts
+++ src/slices/fishboneSlice.ts
@@ -155,76 +155,7 @@ const initialState: FishboneState = {
 export const fishboneSlice = createSlice({
   name: "fishbone",
   initialState,
-  reducers: {
-    // updateProblem: (state, action: PayloadAction<string>) => {
-    //   state.problem = action.payload;
-    // },
-    // updateCause: (
-    //   state,
-    //   action: PayloadAction<{ id: number; newName: string; newNotes: string }>
-    // ) => {
-    //   if (state.diagram?.causes) {
-    //     const cause = findCauseById(state.diagram.causes, action.payload.id);
-    //     if (cause) {
-    //       cause.name = action.payload.newName;
-    //       cause.notes = action.payload.newNotes;
-    //     }
-    //   }
-    // },
-    // addCause: (
-    //   state,
-    //   action: PayloadAction<{
-    //     parentId: number;
-    //     newCauseName: string;
-    //     notes?: string;
-    //   }>
-    // ) => {
-    //   if (state.diagram?.causes) {
-    //     const parent = findCauseById(state.diagram.causes, action.payload.parentId);
-
-    //     if (parent) {
-    //       const newCause: Cause = {
-    //         id: Math.floor(Math.random() * 10000000) + 1, // Should be dictated by server
-    //         name: action.payload.newCauseName,
-    //         causes: [],
-    //       };
-    //       parent.causes = [...(parent.causes || []), newCause];
-    //     }
-    //   }
-    // },
-    // addCauseCategory: (
-    //   state,
-    //   action: PayloadAction<{ newCauseCategoryName: string; notes?: string }>
-    // ) => {
-    //   if (!state.diagram) {
-    //     state.diagram = {
-    //       id: "",
-    //       problem: "",
-    //       causes: []
-    //     };
-    //   }
-      
-    //   const newCauseCategory: Cause = {
-    //     id: Math.floor(Math.random() * 10000000) + 1, // Should be dictated by server
-    //     name: action.payload.newCauseCategoryName,
-    //     notes: action.payload.notes,
-    //     causes: [],
-    //   };
-    //   state.diagram.causes = [...state.diagram.causes, newCauseCategory];
-    // },
-    // deleteCause: (state, action: PayloadAction<{ id: number }>) => {
-    //   const removeCause = (causes: Cause[]): Cause[] =>
-    //     causes.filter((c) => {
-    //       if (c.id === action.payload.id) return false;
-    //       if (c.causes) c.causes = removeCause(c.causes);
-    //       return true;
-    //     });
-
-    //   if (state.diagram?.causes) {
-    //     state.diagram.causes = removeCause(state.diagram.causes);
-    //   }
-    // },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchDiagram.pending, (state) => {
@@ -298,9 +229,5 @@ export const fishboneSlice = createSlice({
       });
   },
 });
-
-// export const {
-//   deleteCause,
-// } = fishboneSlice.actions;
 
 export default fishboneSlice.reducer;
